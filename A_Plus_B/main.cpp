4317e8d46f9a076860c8b1e2d597cd5a996c846d
Robert Valentic <robert_val1@hotmail.com>
2019-02-11 19:29:35 +0800

    Added 3 lines of code to increase runtime, and replaced the arrays with vectors to hold the inputs, as well as an introductory comment block.

--- A_Plus_B/main.cpp
+++ A_Plus_B/main.cpp
@@ -1,20 +1,35 @@
+/* 
+ * Robert Valentic
+ * 11/02/2019
+ * 
+ * Updated solution to pcs.org.au "A Plus B" problem.
+ * This one includes 3 lines to decrease program run time, and the inclusion
+ * of vectors.
+ * 
+ */
+ 
 #include <iostream>
+#include <vector>
 
-int main(){
-    int number_of_lines;
+int main()
+{
+    std::ios_base::sync_with_stdio (false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
     
+    int number_of_lines;
     std::cin >> number_of_lines;
     
-    int left_collumn [number_of_lines];
-    int right_collumn [number_of_lines];
+    std::vector<int> left_collumn (number_of_lines);
+    std::vector<int> right_collumn (number_of_lines);
     
     for (int i = 0 ; i < number_of_lines ; i++){
-        std::cin >> left_collumn[i];
-        std::cin >> right_collumn[i];
+        std::cin >> left_collumn.at(i);
+        std::cin >> right_collumn.at(i);
     }
     
     for (int i = 0 ; i < number_of_lines ; i++){
-        std::cout << left_collumn[i] + right_collumn[i] << "\n";
+        std::cout << left_collumn.at(i) + right_collumn.at(i) << "\n";
     }
     
     return 0;
